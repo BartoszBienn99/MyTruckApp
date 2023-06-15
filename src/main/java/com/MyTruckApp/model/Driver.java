2b3209af85cb53e7ebc0bbbfcf23c97e58fe58b8
BartoszBienn <bartosz.bienn@gmail.com>
2023-06-15 13:43:48 +0200

    added Driver model

---
+++ src/main/java/com/MyTruckApp/model/Driver.java
@@ -0,0 +1,68 @@
+package com.MyTruckApp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "drivers")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String driverFirstName;
+    private String driverLastName;
+    private boolean isFree;
+
+    public Driver(int id, String driverFirstName, String driverLastName, boolean isFree) {
+        this.id = id;
+        this.driverFirstName = driverFirstName;
+        this.driverLastName = driverLastName;
+        this.isFree = isFree;
+    }
+
+    public Driver() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDriverFirstName() {
+        return driverFirstName;
+    }
+
+    public void setDriverFirstName(String driverFirstName) {
+        this.driverFirstName = driverFirstName;
+    }
+
+    public String getDriverLastName() {
+        return driverLastName;
+    }
+
+    public void setDriverLastName(String driverLastName) {
+        this.driverLastName = driverLastName;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public void setFree(boolean free) {
+        isFree = free;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "id=" + id +
+                ", driverFirstName='" + driverFirstName + '\'' +
+                ", driverLastName='" + driverLastName + '\'' +
+                ", isFree=" + isFree +
+                '}';
+    }
+}
