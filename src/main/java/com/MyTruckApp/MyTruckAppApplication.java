7394685e42254861850ba8e326ba17b550298bf8
BartoszBienn <bartosz.bienn@gmail.com>
2023-06-15 13:36:20 +0200

    initialize app

---
+++ src/main/java/com/MyTruckApp/MyTruckAppApplication.java
@@ -0,0 +1,13 @@
+package com.MyTruckApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTruckAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTruckAppApplication.class, args);
+	}
+
+}
